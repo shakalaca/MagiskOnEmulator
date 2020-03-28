61b6e7792a2aaa7417db86c260f4a4a7a1dd01f3
Shaka Huang <shakalaca@gmail.com>
2020-03-28 14:27:39 +0800

    Refactor
    * Support both arm & x86
    * Use 64bit binary for 64bit system
    *
    
    Signed-off-by: Shaka Huang <shakalaca@gmail.com>

--- process.sh
+++ process.sh
@@ -1,49 +1,54 @@
 #!/system/bin/sh
 
-TMP_DIR=/data/local/tmp/tmp
-MAGISK_DIR=/data/local/tmp/magisk
+BASE_DIR=$1
+TMP_DIR=$BASE_DIR/tmp
+MAGISK_DIR=$BASE_DIR/magisk
+RAMDISK=$BASE_DIR/ramdisk.img
 
-# setup environment
-cd /data/local/tmp
-mkdir $TMP_DIR
-mkdir $MAGISK_DIR
-chmod 755 busybox
-./busybox gzip -fd ramdisk.img.gz
-mv ramdisk.img $MAGISK_DIR
+mkdir -p $TMP_DIR
+mkdir -p $MAGISK_DIR
 
-# extract files
-./busybox unzip magisk.zip -d $TMP_DIR
-
-pm install -r $TMP_DIR/common/magisk.apk
-rm $TMP_DIR/common/magisk.apk
-
-mv $TMP_DIR/x86/* $MAGISK_DIR
-mv $TMP_DIR/common/* $MAGISK_DIR
-mv $TMP_DIR/chromeos $MAGISK_DIR
-mv $TMP_DIR/META-INF/com/google/android/update-binary $MAGISK_DIR
-
-cd $MAGISK_DIR
-chmod 755 *
+# prepare busybox
+cd $BASE_DIR
 sh update-binary -x
 
-# do basic checks
+# platform check
 API=`getprop ro.build.version.sdk`
+ABI=`getprop ro.product.cpu.abi | ./busybox cut -c-3`
+ABI2=`getprop ro.product.cpu.abi2 | ./busybox cut -c-3`
+ABILONG=`getprop ro.product.cpu.abi`
 
-# extract magisk
-./magiskinit -x magisk magisk
+ARCH=arm
+IS64BIT=false
+if [ "$ABI" = "x86" ]; then ARCH=x86; fi;
+if [ "$ABI2" = "x86" ]; then ARCH=x86; fi;
+if [ "$ABILONG" = "arm64-v8a" ]; then ARCH=arm; IS64BIT=true; fi;
+if [ "$ABILONG" = "x86_64" ]; then ARCH=x86; IS64BIT=true; fi;
+  
+# extract files
+./busybox gzip -fd ${RAMDISK}.gz
+./busybox unzip magisk.zip -d $TMP_DIR
 
-# install magiskinit
+mv $TMP_DIR/$ARCH/* $MAGISK_DIR
+mv $TMP_DIR/common/* $MAGISK_DIR
+mv $TMP_DIR/chromeos $MAGISK_DIR
+$IS64BIT && mv -f $MAGISK_DIR/magiskinit64 $MAGISK_DIR/magiskinit || rm -f $MAGISK_DIR/magiskinit64
+
+chmod 755 $MAGISK_DIR/*
+$MAGISK_DIR/magiskinit -x magisk $MAGISK_DIR/magisk
+
+# patch ramdisk
 echo "KEEPVERITY=false" >> config
 echo "KEEPFORCEENCRYPT=true" >> config
-./magiskboot cpio ramdisk.img "mkdir 000 .backup" "mv init .backup/init" 
-./magiskboot cpio ramdisk.img "add 750 init magiskinit" "add 000 .backup/.magisk config"
-KEEPVERITY=false KEEPFORCEENCRYPT=true ./magiskboot cpio ramdisk.img patch
-gzip ramdisk.img
-mv ramdisk.img.gz /data/local/tmp/ramdisk.img
+$MAGISK_DIR/magiskboot cpio $RAMDISK "mkdir 000 .backup" "mv init .backup/init" 
+$MAGISK_DIR/magiskboot cpio $RAMDISK "add 750 init $MAGISK_DIR/magiskinit" "add 000 .backup/.magisk config"
+KEEPVERITY=false KEEPFORCEENCRYPT=true $MAGISK_DIR/magiskboot cpio $RAMDISK patch
+./busybox gzip $RAMDISK
+mv ${RAMDISK}.gz $RAMDISK
 
-# clean up
-rm -f config
-rm -f update-binary
+# install apk
+pm install -r $MAGISK_DIR/magisk.apk
+rm -f $MAGISK_DIR/magisk.apk
 
 # move files
 INSTALL_PATH=/data/user_de/0/com.topjohnwu.magisk/install/
@@ -53,5 +58,7 @@ fi
 run-as com.topjohnwu.magisk mkdir $INSTALL_PATH
 run-as com.topjohnwu.magisk cp -r $MAGISK_DIR/* $INSTALL_PATH
 
+# cleanup
+rm -f config
 rm -rf $TMP_DIR
 rm -rf $MAGISK_DIR
