046dab249779be2b58fb15b6c97b2fe38349b9ee
Shaka Huang <shakalaca@gmail.com>
2020-04-02 17:57:39 +0800

    Add script for pulling initrd.img and ramdisk.img
    Signed-off-by: Shaka Huang <shakalaca@gmail.com>

---
+++ pull_image.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+adb wait-for-device
+adb -e shell "mkdir /data/local/tmp/root"
+adb -e shell su -c "mount /dev/block/sda1 /data/local/tmp/root"
+adb -e shell "cp /data/local/tmp/root/*/initrd.img /data/local/tmp"
+adb -e shell "cp /data/local/tmp/root/*/ramdisk.img /data/local/tmp"
+adb -e shell su -c "umount /data/local/tmp/root"
+adb -e shell "rmdir /data/local/tmp/root"
+adb -e pull /data/local/tmp/initrd.img
+adb -e pull /data/local/tmp/ramdisk.img
+
