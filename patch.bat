c30a30ef0eefaba321f0d2be6aac928bbda76c0c
Shaka Huang <shakalaca@gmail.com>
2020-04-02 17:45:36 +0800

    Since magisk.zip is custom build, rename to avoid confusion
    Signed-off-by: Shaka Huang <shakalaca@gmail.com>

--- patch.bat
+++ patch.bat
@@ -2,7 +2,7 @@
 
 adb wait-for-device;
 adb -e push ramdisk.img "/data/local/tmp/ramdisk.img.gz";
-adb -e push magisk.zip "/data/local/tmp/";
+adb -e push magisk_emu.zip "/data/local/tmp/magisk.zip";
 adb -e push update-binary "/data/local/tmp/";
 adb -e push process.sh "/data/local/tmp/";
 adb -e shell "sh /data/local/tmp/process.sh /data/local/tmp";
