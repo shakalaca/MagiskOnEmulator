f8bbade8a3b98c94e544c87b598b8d3a1368483b
Shaka Huang <shakalaca@gmail.com>
2020-04-04 01:24:17 +0800

    Adjust scripts
    Signed-off-by: Shaka Huang <shakalaca@gmail.com>

--- install_vbox.sh
+++ install_vbox.sh
@@ -2,4 +2,4 @@
 
 adb wait-for-device
 adb -e push install.sh /data/local/tmp
-adb -e shell su -c "sh /data/local/tmp/install.sh /data/local/tmp"
+adb -e shell "su -c 'sh /data/local/tmp/install.sh /data/local/tmp'"
--- patch_vbox.bat
+++ patch_vbox.bat
@@ -1,8 +1,8 @@
 @echo off
 
-adb wait-for-device;
-adb -e push magisk.zip "/data/local/tmp/";
-adb -e push update-binary "/data/local/tmp/";
-adb -e push process.sh "/data/local/tmp/";
-adb -e push initrd.patch "/data/local/tmp/";
-adb -e shell "sh /data/local/tmp/process.sh /data/local/tmp";
+adb wait-for-device
+adb -e push magisk.zip "/data/local/tmp/"
+adb -e push update-binary "/data/local/tmp/"
+adb -e push process.sh "/data/local/tmp/"
+adb -e push initrd.patch "/data/local/tmp/"
+adb -e shell "sh /data/local/tmp/process.sh /data/local/tmp"
