f52925567066357a0ebe743a58b7abc1ecec919c
Shaka Huang <shakalaca@gmail.com>
2020-06-12 09:41:13 +0800

    Fix error patching ramdisk on Windows
    process.sh may in dos-style format, convert it before executing.
    
    Fix #2, credit to @fusionneur

--- patch.bat
+++ patch.bat
@@ -5,6 +5,7 @@ adb -e push ramdisk.img /data/local/tmp/ramdisk.img.gz
 adb -e push magisk_emu.zip /data/local/tmp/magisk.zip
 adb -e push update-binary /data/local/tmp
 adb -e push process.sh /data/local/tmp
+adb -e shell "dos2unix /data/local/tmp/process.sh"
 adb -e shell "sh /data/local/tmp/process.sh /data/local/tmp"
 adb -e pull /data/local/tmp/ramdisk.img
 
