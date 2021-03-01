5d88b621a700a13120f05d11e61ed74922dd56c5
Shaka Huang <shakalaca@gmail.com>
2021-03-01 23:23:55 +0800

    Support argument in vbox patch

--- patch_vbox.sh
+++ patch_vbox.sh
@@ -7,4 +7,4 @@ adb -e push magisk.zip /data/local/tmp/
 adb -e push busybox /data/local/tmp/
 adb -e push process.sh /data/local/tmp/
 adb -e push initrd.patch /data/local/tmp/
-adb -e shell "sh /data/local/tmp/process.sh /data/local/tmp"
+adb -e shell "sh /data/local/tmp/process.sh /data/local/tmp $1"
