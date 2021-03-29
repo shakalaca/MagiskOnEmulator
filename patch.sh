5f32e63acdf8d58967b5edaabed02f9ae448ac77
Phuong Nguyen <phuongnd08@gmail.com>
2021-03-29 08:43:40 +0700

    Push busybox-arm64-selinux to device

--- patch.sh
+++ patch.sh
@@ -3,7 +3,7 @@
 adb wait-for-device
 adb -e push ramdisk.img /data/local/tmp/ramdisk.img.gz
 adb -e push magisk.zip /data/local/tmp/magisk.zip
-adb -e push busybox /data/local/tmp/
+adb -e push busybox-arm64-selinux /data/local/tmp/busybox
 adb -e push process.sh /data/local/tmp/
 adb -e shell "dos2unix /data/local/tmp/process.sh"
 adb -e shell "sh /data/local/tmp/process.sh /data/local/tmp $1"
