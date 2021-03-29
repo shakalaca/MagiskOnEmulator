9c02ea02619ba2b8e7b1eb49a147e2832c47ff73
Phuong Nguyen <phuongnd08@gmail.com>
2021-03-29 08:44:31 +0700

    Only support *nix environment

--- patch_vbox.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-adb wait-for-device
-adb -e push magisk.zip "/data/local/tmp/"
-adb -e push busybox "/data/local/tmp/"
-adb -e push process.sh "/data/local/tmp/"
-adb -e push initrd.patch "/data/local/tmp/"
-adb -e shell "dos2unix /data/local/tmp/process.sh"
-adb -e shell "sh /data/local/tmp/process.sh /data/local/tmp %1"
