dac8264a69dee3ecf8d35cfac7370f65836357b0
Phuong Nguyen <phuongnd08@gmail.com>
2021-03-29 10:17:03 +0700

    Use prebuilt arm64 magisk files

--- patch.sh
+++ patch.sh
@@ -4,6 +4,9 @@ adb wait-for-device
 adb -e push ramdisk.img.original /data/local/tmp/ramdisk.img.gz
 adb -e push magisk.zip /data/local/tmp/magisk.zip
 adb -e push busybox-arm64-selinux /data/local/tmp/busybox
+adb -e push magisk-arm64 /data/local/tmp/magisk-bin
+adb -e push magiskboot-arm64 /data/local/tmp/magiskboot
+adb -e push magiskinit-arm64 /data/local/tmp/magiskinit
 adb -e push process.sh /data/local/tmp/
 adb -e shell "dos2unix /data/local/tmp/process.sh"
 adb -e shell "sh /data/local/tmp/process.sh /data/local/tmp $1"
