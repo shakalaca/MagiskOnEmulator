c8da25b389e502e38ad57bbce218b1ec7dc2712f
Phuong Nguyen <phuongnd08@gmail.com>
2021-03-29 09:19:06 +0700

    Pull the processed ramdisk as .patched

--- patch.sh
+++ patch.sh
@@ -7,4 +7,4 @@ adb -e push busybox-arm64-selinux /data/local/tmp/busybox
 adb -e push process.sh /data/local/tmp/
 adb -e shell "dos2unix /data/local/tmp/process.sh"
 adb -e shell "sh /data/local/tmp/process.sh /data/local/tmp $1"
-adb -e pull /data/local/tmp/ramdisk.img
+adb -e pull /data/local/tmp/ramdisk.img ramdisk.img.patched
